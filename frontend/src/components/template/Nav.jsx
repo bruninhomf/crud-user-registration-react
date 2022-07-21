be309e4952d66b20311359125ec6067c35cf6877
Bruno <bruno.firmiano@acct.global>
2022-07-20 21:52:58 -0300

    Nav style

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import './Nav.css'
+import { Link } from 'react-router-dom'
 
 export default props => 
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            <Link to="/">
+                <i className="fa fa-home"> Início</i>
+            </Link>
+            <Link to="/users">
+                <i className="fa fa-users"> Usuários</i>
+            </Link>
+        </nav>
     </aside>
